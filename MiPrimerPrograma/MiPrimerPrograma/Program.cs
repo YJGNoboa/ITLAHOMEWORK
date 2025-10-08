a8c39cfa02fcf9d25bfa4a28286b79ee2a6cf4c3
Yanel Gonzalez <yanelgonzalez752@gmail.com>
2025-10-08 02:36:15 -0400

    My first Proyect

---
+++ MiPrimerPrograma/MiPrimerPrograma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Headers;
+
+namespace MiPrimerPrograma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.Write("Escribe la medida de un cateto: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Escribe la medida del otro cateto: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            double Hipotenusa = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            Console.WriteLine("La hipotenusa es: " + Hipotenusa);
+        }
+    }
+}
---
+++ MiPrimerPrograma/Perimetro/Program.cs
@@ -0,0 +1,14 @@
+﻿int sidesPolygon;
+double sidesSizes;
+double perimeter;
+
+Console.WriteLine("How many sides does the polygon have?");
+sidesPolygon = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("How many size one sides");
+sidesSizes = Convert.ToDouble(Console.ReadLine());
+
+perimeter = sidesSizes * sidesPolygon;
+
+Console.WriteLine(perimeter);   
+
+
