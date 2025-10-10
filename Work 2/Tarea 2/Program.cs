e3707d81a88e57e5a4d7db2e8d880ce670927d89
Yanel Gonzalez <yanelgonzalez752@gmail.com>
2025-10-10 14:28:41 -0400

    Update Program.cs

--- Work 2/Tarea 2/Program.cs
+++ Work 2/Tarea 2/Program.cs
@@ -1,14 +1,14 @@
 ﻿int num;
 //Yanel Josias Gonzalez Noboa 2025-0908
-Console.Write("Enter a number for know if pair or not pair \n");
+Console.Write("Enter a number for know if even or not even \n");
 num = Convert.ToInt32(Console.ReadLine());
 
 if (num % 2 == 0)
 {
-    Console.WriteLine("The number You have Introduced is pair");
+    Console.WriteLine("The number You have Introduced is even");
 }
 else
 {
-    Console.WriteLine("The nummber You have Introtuced isn't pair");
+    Console.WriteLine("The nummber You have Introtuced isn't even");
 }
 Console.ReadKey();
